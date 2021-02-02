a769abd2cac1d7499bde1f1a199fde5a48b8676d
Wilson Ng <github email address>
2021-02-02 15:34:23 -0500

    assignment 1 started

---
+++ week-one/Assignment1.sql
@@ -0,0 +1,10 @@
+-- 1. How many airplanes have listed speeds? 
+-- What is the minimum listed speed and the maximum listed speed?
+
+-- 2. What is the total distance flown by all of the planes in January 2013?
+-- What is the total distance flown by all of the planes
+
+-- 3. What is the total distance flown for all planes on July 5, 2013 grouped by aircraft manufacturer?
+-- Write this statement first using an INNER JOIN, then using a LEFT OUTER JOIN. How do your results compare?
+
+-- 4. Write and answer at least one question of your own choosing that joins information from at least three of the tables in the flights database.
